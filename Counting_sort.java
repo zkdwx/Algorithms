f861920b99f3a27397f1bc8fd44435d018288fb1
zkdwx <45314224+zkdwx@users.noreply.github.com>
2019-03-20 17:39:45 +0800

    Add files via upload

---
+++ Counting_sort.java
@@ -0,0 +1,37 @@
+/**
+ * @time 2019-2-20-17:36
+ * @author 王兴
+  *《算法导论第三版》
+  *第八章：计数排序
+ */
+public class Counting_sort {
+	public static void counting_sort(int[] A,int[] B,int k) {
+		int[] C=new int[k];                         
+		for (int i = 0; i < k; i++) {
+			C[i]=0;
+		}
+		for (int j = 1; j < A.length; ) {
+			
+			C[A[j]]=C[A[j]]+1;
+			j++;
+		}
+		for (int i = 1; i < k; i++) {
+			C[i]=C[i]+C[i-1];
+		}
+		for (int j = A.length-1; j >0;) {
+			B[C[A[j]]]=A[j];
+			C[A[j]]=C[A[j]]-1;
+			 j--;
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] A= {4,2,5,3,0,2,3,0,3};
+		int[] B=new int[9];
+		counting_sort(A, B,6);
+		for (int i = 1; i < B.length; i++) {
+			System.out.print(B[i]);
+		}
+	}
+
+}
